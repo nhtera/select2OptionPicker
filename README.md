d314a8222b38fe7d015b6effcf50edc01601bc44
tien nguyen <nguyentien8x@gmail.com>
2013-10-27 17:53:03 +0700

    First release

--- README.md
+++ README.md
@@ -2,3 +2,81 @@ select2OptionPicker
 ===================
 
 Jquery plugin convert standard html select into list ui to picker
+
+## About
+
+select2OptionPicker is a jQuery plugin that provides an alternative to the standard html select. The plugin converts selects into a series of li like elements, 
+and if used correctly can provide a better visual representation of the options available to a user.
+
+### Features
+
+* Converts normal HTML selects into button like elements (a tags)
+* Supports JS change callbacks
+* Supports image and color options
+* Supports disabled options
+* Supports disabled selects
+* Supports OptGroups
+* Allows no default selection (like threadless)
+
+## Howto
+
+Add a standard select to the page and then make 1 jQuery call to convert it into buttons:
+
+    <select name="simple-select">
+      <option>One</option>
+      <option>Two</option>
+      <option>Three</option>
+    </select>
+
+    <script>
+       $('select[name=simple-select]').select2OptionPicker();
+    </script>
+
+When the user clicks on one of the buttons (their really links), the plugin sets that value in your select so server side your code should be exactly the same.
+
+### Image & color
+
+	<select name="adv-select" class="standard-demo">
+          <option data-img-src="http://i.imgur.com/fBJ88eD.png">One</option>
+          <option data-color="#C978D7">Two</option>
+          <option>Three</option>
+        </select>
+
+    <script>
+       $('select[name=adv-select]').select2OptionPicker();
+    </script>
+
+### Javascript Callbacks
+Any change events registered against the original select will also fire when the select buttons are clicked. e.g.
+
+      $('select[name=simple-select]').change(function() {
+        alert('Changed to ' + $(this).val());
+      });
+
+### No Default Selection
+By default the option selected in your select will be marked as selected
+in the buttons.  However, the plugin also supports the threadless style,
+no default selection.
+
+       $('select[name=simple-select]').select2Buttons({noDefault: true});
+
+### OptGroups
+The plugin understands and honors option groups.  Each option group will
+be placed in a seperate UL and the optgroups label will be added to the
+page as a strong tag.
+
+### Disabling
+The plugin supports disabled options and disabled selects.
+
+### Styling
+The select2Buttons.css is provided as a guide, it's mostly copied from
+threadless.com.  I suggest restyling if your going to use the plugin on a
+commercial site.
+
+## Licencing
+
+MIT
+
+## Contact
+
+TienNH - nguyentien8x(at)gmail.com
---
+++ jQuery.select2OptionPicker.js
@@ -0,0 +1,86 @@
+/**
+ * .select2OptionPicker - Convert standard html select into list ui to picker
+ *
+ * Version: 1.0.0
+ * Updated: 2013-10-27
+ *
+ *  Provides an alternative look and feel for HTML select buttons
+ *
+ * Author: TienNH (nguyentien8x@gmail.com)
+ *
+ * Licensed under the MIT
+ **/
+jQuery.fn.select2OptionPicker = function(options) {
+  return this.each(function(){
+    var $ = jQuery;
+    var select = $(this);
+    var multiselect = select.attr('multiple');
+    select.hide();
+
+    var buttonsHtml = $('<div class="select2OptionPicker"></div>');
+    var selectIndex = 0;
+    var addOptGroup = function(optGroup){
+      if (optGroup.attr('label')){
+        buttonsHtml.append('<strong>' + optGroup.attr('label') + '</strong>');
+      }
+      var ulHtml =  $('<ul class="select-buttons">');
+      optGroup.children('option').each(function(){
+      	var img_src = $(this).data('img-src');
+      	var color = $(this).data('color');
+      	
+        var liHtml = $('<li></li>');
+        if ($(this).attr('disabled') || select.attr('disabled')){
+          liHtml.addClass('disabled');
+          liHtml.append('<span>' + $(this).html() + '</span>');
+        }else{
+        	
+	      	if(color){
+	      		liHtml.append('<a href="#" style="background-color:' + color + '" data-select-index="' + selectIndex + '">&nbsp;</a>');
+	      	}else if(img_src){
+      		 	liHtml.append('<a href="#" data-select-index="' + selectIndex + '"><img class="image_picker" src="' + img_src + '"></a>');
+	      	}else {
+          		liHtml.append('<a href="#" data-select-index="' + selectIndex + '">' + $(this).html() + '</a>');
+      		}
+        }
+
+        // Mark current selection as "picked"
+        if((!options || !options.noDefault) && $(this).attr('selected')){
+          liHtml.children('a, span').addClass('picked');
+        }
+        ulHtml.append(liHtml);
+        selectIndex++;
+      });
+      buttonsHtml.append(ulHtml);
+    }
+
+    var optGroups = select.children('optgroup');
+    if (optGroups.length == 0) {
+      addOptGroup(select);
+    }else{
+      optGroups.each(function(){
+        addOptGroup($(this));
+      });
+    }
+
+    select.after(buttonsHtml);
+
+    buttonsHtml.find('a').click(function(e){
+      e.preventDefault();
+      var clickedOption = $(select.find('option')[$(this).attr('data-select-index')]);
+      if(multiselect){
+        if(clickedOption.attr('selected')){
+          $(this).removeClass('picked');
+          clickedOption.removeAttr('selected');
+        }else{
+          $(this).addClass('picked');
+          clickedOption.attr('selected', 'selected');
+        }
+      }else{
+        buttonsHtml.find('a, span').removeClass('picked');
+        $(this).addClass('picked');
+        clickedOption.attr('selected', 'selected');
+      }
+      select.trigger('change');
+    });
+  });
+};
